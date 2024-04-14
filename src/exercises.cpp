0849b28f244c5fca1d23028b2b221a549e1c55e2
EduardoZuritaz <159577059+EduardoZuritaz@users.noreply.github.com>
2024-04-13 22:03:58 -0400

    exercise3.1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -31,7 +31,19 @@ void exercise_2(string s1) {
 }
 
 void exercise_3(string s1) {
-   // TODO: YOUR CODE HERE
+  for (int i = 0; i < s1.size(); i++){
+    char word = s1[i];
+    if (i > 19){
+        break;
+    }
+    if (word == '0'){
+        cout << "No cake :(" << endl;
+        break;
+    }
+    else if (word == '1'){
+        cout << "Om-nom-nom :P" << endl;
+    }
+  }
 }
 
 void exercise_4(int n) {
