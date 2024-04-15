860cd2bb878f9c17c4bddabbebd911675c08d241
EduardoZuritaz <159577059+EduardoZuritaz@users.noreply.github.com>
2024-04-14 21:57:50 -0400

    exercise12.4

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -117,7 +117,7 @@ void exercise_12() {
   double v = sec2;
   for(int i = 1; 10 >= i; ++i){
     int Vn = v;
-    double result = sec2/(i + 1 );
+    double result = sec2/(i + 1);
     cout << "U" << i << " = " << sec2 << " " << i << " = " << Vn << endl;
     sec2 = result;
   }
